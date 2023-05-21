431d96e514216abcebd0458c30b1558988c985d0
Enzo SIMON <esimon.gap@gmail.com>
2023-05-21 19:07:00 +0200

    fix the new Catalogue page

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Products;
-use App\Entity\Categories;
 use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,14 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductsController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(Products $product, Categories $category, ProductsRepository $productsRepository, Request $request): Response
+    public function index(Products $product, ProductsRepository $productsRepository): Response
     {
-        $page = $request->query->getInt('page', 1);
+        $products = $productsRepository->findAll();
 
-        //On va chercher la liste des produits de la catégorie
-        $products = $productsRepository->findProductsPaginated($page, $category->getSlug(), 4);
-
-        return $this->render('products/index.html.twig', compact('category', 'products'));
+        return $this->render('products/index.html.twig', compact('products'));
     }
 
     #[Route('/{slug}', name: 'details')]
