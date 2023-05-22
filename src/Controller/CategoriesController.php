d4cc0ad50912d2a640576a1d867876800c50e807
Enzo SIMON <esimon.gap@gmail.com>
2023-05-23 01:27:01 +0200

    fix promo problem on category page

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,9 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Entity\Products;
 use App\Entity\Promotions;
-use App\Repository\ProductsRepository;
-use App\Repository\PromotionsRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,21 +16,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $category, ProductsRepository $productsRepository, ManagerRegistry $managerRegistry, Request $request): Response
+    public function list(Categories $category, ManagerRegistry $managerRegistry, Request $request): Response
     {
         //On va chercher le numéro de page dans l'url
         $page = $request->query->getInt('page', 1);
 
         //On va chercher la liste des produits de la catégorie et les promotions
         $entityManager = $managerRegistry->getManager();
-        $products = $productsRepository->findProductsPaginated($page, $category->getSlug(), 4);
+        $products = $entityManager->getRepository(Products::class)->findAll();
         $promotions = $entityManager->getRepository(Promotions::class)->findAll();
 
         return $this->render('categories/list.html.twig', compact('category', 'products', 'promotions'));
-        // Syntaxe alternative
-        // return $this->render('categories/list.html.twig', [
-        //     'category' => $category,
-        //     'products' => $products
-        // ]);
     }
 }
