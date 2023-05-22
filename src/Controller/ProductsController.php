36b871448d78da63c3e826e8937113aca27c53c9
Enzo SIMON <esimon.gap@gmail.com>
2023-05-22 21:20:20 +0200

    show promotion on Catalogue page

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -8,23 +8,26 @@ use App\Entity\Promotions;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/produits', name: 'products_')]
 class ProductsController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(ManagerRegistry $managerRegistry): Response
+    public function index(ManagerRegistry $managerRegistry, Request $request): Response
     {
         $entityManager = $managerRegistry->getManager();
         $products = $entityManager->getRepository(Products::class)->findAll();
         $promotions = $entityManager->getRepository(Promotions::class)->findAll();
         $categories = $entityManager->getRepository(Categories::class)->findAll();
+        $selectedCategoryId = $request->query->get('category');
 
         return $this->render('products/index.html.twig', [
             'products' => $products,
             'promotions' => $promotions,
             'categories' => $categories,
+            'selectedCategory' => $selectedCategoryId,
         ]);
     }
 
