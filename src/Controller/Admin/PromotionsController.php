bd34c884b95058d5673b8c28d074290b92da303c
Enzo SIMON <esimon.gap@gmail.com>
2023-05-23 02:06:40 +0200

    fix promotion delete btn

--- src/Controller/Admin/PromotionsController.php
+++ src/Controller/Admin/PromotionsController.php
@@ -17,15 +17,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 
 #[Route('/admin/promotion', name: 'admin_promotion_')]
 class PromotionsController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(ProductsRepository $productsRepository): Response
+    public function index(ProductsRepository $productsRepository, ManagerRegistry $managerRegistry): Response
     {
+        $entityManager = $managerRegistry->getManager();
+        $promotions = $entityManager->getRepository(Promotions::class)->findAll();
         $produits = $productsRepository->findAll();
-        return $this->render('admin/products/index.html.twig', compact('produits'));
+        return $this->render('admin/products/index.html.twig', compact('produits', 'promotions'));
     }
 
     #[Route('/ajout/{id}', name: 'ajout')]
@@ -70,68 +73,6 @@ class PromotionsController extends AbstractController
         // ['promotionForm' => $promotionForm]
     }
 
-    // #[Route('/edition/{id}', name: 'edit')]
-    // public function edit(Products $product, Request $request, EntityManagerInterface $em, SluggerInterface $slugger, PictureService $pictureService): Response
-    // {
-    //     // On vérifie si l'utilisateur peut éditer avec le Voter
-    //     $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product);
-
-    //     // On divise le prix par 100
-    //     // $prix = $product->getPrice() / 100;
-    //     // $product->setPrice($prix);
-
-    //     // On crée le formulaire
-    //     $promotionForm = $this->createForm(ProductsFormType::class, $product);
-
-    //     // On traite la requête du formulaire
-    //     $promotionForm->handleRequest($request);
-
-    //     //On vérifie si le formulaire est soumis ET valide
-    //     if($promotionForm->isSubmitted() && $promotionForm->isValid()){
-    //         // On récupère les images
-    //         $images = $promotionForm->get('images')->getData();
-
-    //         foreach($images as $image){
-    //             // On définit le dossier de destination
-    //             $folder = 'products';
-
-    //             // On appelle le service d'ajout
-    //             $fichier = $pictureService->add($image, $folder, 300, 300);
-
-    //             $img = new Images();
-    //             $img->setName($fichier);
-    //             $product->addImage($img);
-    //         }
-            
-            
-    //         // On génère le slug
-    //         $slug = $slugger->slug($product->getName());
-    //         $product->setSlug($slug);
-
-    //         // On arrondit le prix 
-    //         // $prix = $product->getPrice() * 100;
-    //         // $product->setPrice($prix);
-
-    //         // On stocke
-    //         $em->persist($product);
-    //         $em->flush();
-
-    //         $this->addFlash('success', 'Produit modifié avec succès');
-
-    //         // On redirige
-    //         return $this->redirectToRoute('admin_products_index');
-    //     }
-
-
-    //     return $this->render('admin/products/edit.html.twig',[
-    //         'promotionForm' => $promotionForm->createView(),
-    //         'product' => $product
-    //     ]);
-
-    //     // return $this->renderForm('admin/products/edit.html.twig', compact('promotionForm'));
-    //     // ['promotionForm' => $promotionForm]
-    // }
-
     #[Route('/suppression/{id}', name: 'delete')]
     public function delete(Promotions $promotion, EntityManagerInterface $em): Response
     {
@@ -142,7 +83,7 @@ class PromotionsController extends AbstractController
         $em->remove($promotion);
         $em->flush();
 
-        $this->addFlash('success', 'Produit supprimé avec succès');
+        $this->addFlash('success', 'Promotion supprimée avec succès');
 
         // Rediriger vers la liste des produits ou une autre page appropriée
         return $this->redirectToRoute('admin_products_index');
